9366a3cbe57e8b129b37ac38df9a58301b0cc721
Mohamed Yassine Ben Yaala <tonemailgmail.com>
2026-04-21 11:18:22 +0000

    cvvcv

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-  output: 'export',
   assetPrefix: '/',   
   trailingSlash: true,
 }
